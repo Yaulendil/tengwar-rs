1a1be6c12d835d06edbe2c2c2bd17cb5105c4083
Yaulendil <davarice@protonmail.com>
2021-02-27 01:46:28 -0500

    Support transcription on Standard Input

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 use itertools::Itertools;
+use std::io::{stdin, BufRead};
 use tengwar::{Quenya, ToTengwar};
 
 
 fn main() {
-    let string_in: String = std::env::args().skip(1)
-        .intersperse(String::from(" "))
-        .collect();
-    println!("{}", string_in.to_tengwar::<Quenya>());
+    let args = std::env::args().skip(1);
+
+    if args.len() > 0 {
+        println!("{}", args.intersperse(String::from(" "))
+            .collect::<String>()
+            .to_tengwar::<Quenya>());
+    } else {
+        let stream = stdin();
+        let mut lines = stream.lock().lines();
+
+        while let Some(Ok(line)) = lines.next() {
+            println!("{}", line.to_tengwar::<Quenya>());
+        }
+    }
 }
