377db174de57b74d8ae7febca1ca7292b87e0ada
Yaulendil <davarice@protonmail.com>
2022-10-31 16:25:23 -0400

    Revise interface of `TengwarMode` trait

--- src/mode.rs
+++ src/mode.rs
@@ -11,10 +11,13 @@ pub use beleriand::Beleriand;
 // pub use general::General;
 pub use gondor::Gondor;
 pub use quenya::Quenya;
+pub use iter::ModeIter;
 
-use crate::{Numeral, Token, TokenIter};
+use crate::{Numeral, Token};
 
 
+/// The result of a call to [`TengwarMode::process`]. This specifies the next
+///     action that will be taken by a [`ModeIter`].
 #[derive(Clone, Debug)]
 pub enum ParseAction {
     /// Nothing could be done with the input.
@@ -27,41 +30,76 @@ pub enum ParseAction {
         len: usize,
     },
     /// A portion of the input has been determined to be irrelevant to the
-    ///     processing. A number of `char`s should be passed through unchanged.
+    ///     processing. The contained number of [`char`]s should be passed
+    ///     through unchanged.
     Skip(usize),
+    /// An escape sequence has been found. The next [`char`] will be ignored,
+    ///     and the one following will be passed through unchanged.
     Escape,
 }
 
 
+/// This trait defines a "mode" of transcription of text into the Tengwar. It
+///     implements methods that receive slices of [`char`]s and progressively
+///     construct [`Token`]s held in internal state.
 pub trait TengwarMode: Default + Sized {
+    /// This is the maximum size for a "chunk" of [`char`]s passed to
+    ///     [`Self::process`]. It is also the maximum number of passes that will
+    ///     be attempted before skipping a `char` and moving on.
     const MAX_CHUNK: usize = 3;
 
-    fn mode_iter(data: Vec<char>) -> iter::ModeIter<Self> {
-        iter::ModeIter::new(data)
+    /// Construct a [`ModeIter`] of this mode to transliterate the input data.
+    fn mode_iter(chars: Vec<char>) -> ModeIter<Self> {
+        ModeIter::new(chars)
     }
 
-    fn token_iter(input: impl AsRef<str>) -> TokenIter<iter::ModeIter<Self>> {
-        let data: Vec<char> = input.as_ref().chars().collect();
-        Self::mode_iter(data).into()
+    /// Set up a [`TokenIter`] over the characters of an input string, and
+    ///     immediately collect it into the target type.
+    ///
+    /// [`TokenIter`]: crate::TokenIter
+    fn transcribe<T: FromIterator<Token>>(input: impl AsRef<str>) -> T {
+        let chars: Vec<char> = input.as_ref().chars().collect();
+        Self::mode_iter(chars).into_token_iter().collect()
     }
 
-    fn tokens(input: impl AsRef<str>) -> Vec<Token> {
-        Self::token_iter(input).collect()
-    }
-
+    /// Try to parse a slice of characters into a [`Numeral`]. If successful,
+    ///     returns the `Numeral` alongside the number of [`char`]s that were
+    ///     processed in order to find it.
+    ///
+    /// The input slice is NOT bounded by [`MAX_CHUNK`], and extends to the end
+    ///     of the data.
+    ///
+    /// [`MAX_CHUNK`]: Self::MAX_CHUNK
     fn find_numeral(&mut self, slice: &[char]) -> Option<(Numeral, usize)> {
         Numeral::parse(slice)
     }
 
+    /// If there is a [`Token`] currently under construction, return it and
+    ///     clear it from the internal state, preparing to begin a new tengwa.
+    ///
+    /// Ideally, this method should be cheap to call, because it will be called
+    ///     whenever [`next`] is called on a [`ModeIter`] which has reached the
+    ///     end of its data.
+    ///
+    /// [`next`]: Iterator::next
     fn finish_current(&mut self) -> Option<Token>;
 
+    /// Process a slice of [`char`]s, and return a [`ParseAction`] indicating
+    ///     the new state of the conversion in progress.
+    ///
+    /// Assuming this method is called by a [`ModeIter`], the input slice will
+    ///     be no more than [`MAX_CHUNK`] in length, but it may be shorter.
+    ///
+    /// [`MAX_CHUNK`]: Self::MAX_CHUNK
     fn process(&mut self, chunk: &[char]) -> ParseAction;
 }
 
 
 #[test]
 fn test_iter() {
-    let iter = quenya::Quenya2::token_iter("yéni 144");
-    let text = iter.collect::<String>();
-    eprintln!("{}", text);
+    fn convert(text: impl AsRef<str>) -> String {
+        quenya::Quenya2::transcribe(text)
+    }
+
+    eprintln!("{}", convert("yéni 144"));
 }
