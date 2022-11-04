8e9deaac7c95d8f70dd38091ea8dc9fd98d6a9f6
Yaulendil <davarice@protonmail.com>
2022-11-04 02:00:04 -0400

    Implement support types around Témar
    Not yet known whether these will be any of use to the main program, but good to have them in the library.

--- src/characters/tema.rs
+++ src/characters/tema.rs
@@ -1,4 +1,7 @@
-/// The Témar are the four series of the primary tengwar. Each Téma is composed
+use std::ops::Index;
+
+
+/// The Témar are the four series of the regular tengwar. Each Téma is composed
 ///     of eight Tyeller, each modifying the tengwa in a different way, and is
 ///     named after its base tengwa.
 ///
@@ -30,4 +33,150 @@ pub struct Tema {
     /// An extended Telco, and two Lúvar.
     ///     Not used in canonical sources.
     pub double_ex: char,
+}
+
+impl Tema {
+    pub const fn tengwa(&self) -> TengwaRegular {
+        TengwaRegular::new(*self)
+    }
+
+    pub const fn get_char(&self, tyelle: Tyelle) -> &char {
+        const Y: bool = true;
+        const N: bool = false;
+        match tyelle {
+            Tyelle { stem_dn: Y, stem_up: N, doubled: N } => &self.single_dn,
+            Tyelle { stem_dn: Y, stem_up: N, doubled: Y } => &self.double_dn,
+            Tyelle { stem_dn: N, stem_up: Y, doubled: N } => &self.single_up,
+            Tyelle { stem_dn: N, stem_up: Y, doubled: Y } => &self.double_up,
+            Tyelle { stem_dn: N, stem_up: N, doubled: Y } => &self.double_sh,
+            Tyelle { stem_dn: N, stem_up: N, doubled: N } => &self.single_sh,
+            Tyelle { stem_dn: Y, stem_up: Y, doubled: N } => &self.single_ex,
+            Tyelle { stem_dn: Y, stem_up: Y, doubled: Y } => &self.double_ex,
+        }
+    }
+}
+
+impl Index<Tyelle> for Tema {
+    type Output = char;
+    fn index(&self, tyelle: Tyelle) -> &Self::Output { self.get_char(tyelle) }
+}
+
+
+/// A small type pairing a [`Tema`] with a specific [`Tyelle`].
+#[derive(Clone, Copy, Debug)]
+pub struct TengwaRegular {
+    pub tema: Tema,
+    pub tyelle: Tyelle,
+}
+
+impl TengwaRegular {
+    pub const fn new(tema: Tema) -> Self {
+        Self { tema, tyelle: Tyelle::new() }
+    }
+
+    pub const fn to_char(&self) -> &char {
+        self.tema.get_char(self.tyelle)
+    }
+
+    pub const fn double(mut self) -> Self {
+        self.tyelle.doubled = true;
+        self
+    }
+
+    pub const fn single(mut self) -> Self {
+        self.tyelle.doubled = false;
+        self
+    }
+
+    pub const fn ascending(mut self) -> Self {
+        self.tyelle.stem_dn = false;
+        self.tyelle.stem_up = true;
+        self
+    }
+
+    pub const fn descending(mut self) -> Self {
+        self.tyelle.stem_dn = true;
+        self.tyelle.stem_up = false;
+        self
+    }
+
+    pub const fn short(mut self) -> Self {
+        self.tyelle.stem_dn = false;
+        self.tyelle.stem_up = false;
+        self
+    }
+
+    pub const fn extended(mut self) -> Self {
+        self.tyelle.stem_dn = true;
+        self.tyelle.stem_up = true;
+        self
+    }
+}
+
+
+/// A small type to represent the specific shape of a regular Tengwa.
+#[derive(Clone, Copy, Debug)]
+pub struct Tyelle {
+    pub stem_dn: bool,
+    pub stem_up: bool,
+    pub doubled: bool,
+}
+
+impl Tyelle {
+    pub const fn new() -> Self {
+        Self {
+            stem_dn: true,
+            stem_up: false,
+            doubled: false,
+        }
+    }
+
+    //  TODO: Would this be of any actual use? Being a bitfield would lead to a
+    //      size of only one byte, while also allowing a Téma to be represented
+    //      as an array with its Tyeller as simple integer indices. However, it
+    //      is hard to imagine that being a significant speedup, even for the
+    //      TeX macro use case.
+    /*pub const fn bits(self) -> u8 {
+        0u8 | self.stem_dn as u8 * 0b001
+            | self.stem_up as u8 * 0b010
+            | self.doubled as u8 * 0b100
+    }*/
+
+    pub const fn double(mut self) -> Self {
+        self.doubled = true;
+        self
+    }
+
+    pub const fn single(mut self) -> Self {
+        self.doubled = false;
+        self
+    }
+
+    pub const fn ascending(mut self) -> Self {
+        self.stem_dn = false;
+        self.stem_up = true;
+        self
+    }
+
+    pub const fn descending(mut self) -> Self {
+        self.stem_dn = true;
+        self.stem_up = false;
+        self
+    }
+
+    pub const fn short(mut self) -> Self {
+        self.stem_dn = false;
+        self.stem_up = false;
+        self
+    }
+
+    pub const fn extended(mut self) -> Self {
+        self.stem_dn = true;
+        self.stem_up = true;
+        self
+    }
+}
+
+impl Default for Tyelle {
+    fn default() -> Self { Self::new() }
 }
