41f99112eb2a130526f0cd3df7cd3414c19086fc
Yaulendil <davarice@protonmail.com>
2022-11-07 03:10:06 -0500

    Trim down benchmarks

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -5,60 +5,7 @@ extern crate test;
 use tengwar::*;
 
 
-const SAMPLES: &[&[char]] = {
-    &[
-        &['c'],
-        &['k'],
-        &['y'],
-        &['w'],
-        &['r', 'd'],
-        &['n', 'g'],
-        &['i'],
-        &['k', 'w'],
-        &['ü'],
-        &['á'],
-        &['a', 'a'],
-        &['ï'],
-        &['ë'],
-        &['í'],
-        &['i', 'i'],
-        &['g'],
-        &['c', 'h'],
-        &['l'],
-        &['l', 'd'],
-        &['q', 'u'],
-        &['q'],
-        &['c', 'w'],
-        &['n', 'g', 'w'],
-        &['n', 'w'],
-        &['s'],
-        &['s', 's'],
-        &['ó'],
-        &['ö'],
-        &['h', 'w'],
-        &['ñ'],
-        &['e'],
-        &['u'],
-        &['é'],
-        &['e', 'e'],
-        &['o', 'o'],
-        &['o'],
-        &['n', 'q', 'u'],
-        &['z'],
-        &['a'],
-        &['ä'],
-        &['n', 'c'],
-        &['ú'],
-        &['u', 'u'],
-    ]
-};
 const TEXTS_Q: &[&str] = &[
-    "tinco parma calma qessë ando umbar anga ungwë\
-    \nþúlë formen harma hwesta anto ampa anca unqë\
-    \nnúmen malta ngoldo ngwalmë orë vala anna wilya\
-    \nrómen arda lambë alda silmë ázë essë\
-    \nhyarmen yanta úrë ossë halla telco ára",
-
 	"ai! laurië lantar lassi súrinen",
 	"yéni únótimë ve rámar aldaron",
 	"yéni ve lintë yuldar avánier",
@@ -92,22 +39,6 @@ const TEXTS_S: &[&str] = &[
 	"le nallon sí di'nguruthos",
 	"a tiro nin , Fanuilos :-",
 ];
-
-
-#[bench]
-fn bench_consonants(b: &mut test::Bencher) {
-    let mut out: Vec<Option<char>> = Vec::with_capacity(SAMPLES.len());
-
-    b.iter(|| {
-        out.clear();
-
-        for slice in SAMPLES {
-            out.push(mode::quenya::consonant_char(slice));
-        }
-    });
-
-    // println!("{:?}", out.into_iter().map(|c| c.unwrap_or('!')).collect::<String>());
-}
 
 
 #[bench]
